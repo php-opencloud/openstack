9505f5da8f737dc1392ba2f7297b8aff976edaa9
Jamie Hannaford <jamie.hannaford@rackspace.com>
2015-03-30 14:18:45 +0200

    Change sample file to use placeholders

--- samples/compute/v2/create_server.php
+++ samples/compute/v2/create_server.php
@@ -1,16 +1,18 @@
 <?php
 
-require __DIR__ . '/../../../vendor/autoload.php';
+require 'vendor/autoload.php';
 
-use OpenStack\OpenStack;
-
-$openstack = new OpenStack;
-
-$compute = $openstack->computeV2([
-    'region' => 'RegionOne',
-    'debug'  => true,
+$openstack = new OpenStack\OpenStack([
+    'username' => '{username}',
+    'password' => '{password}',
+    'tenantId' => '{tenantId}',
+    'authUrl'  => '{authUrl}',
 ]);
 
-$s = $compute->createServer('php_test', 'e37365c2-5c45-4b73-b4ae-828436d5c569', '1');
+$compute = $openstack->computeV2(['region' => '{region}']);
 
-var_dump($s);
+$server = $compute->createServer([
+    'name'     => '{name}',
+    'imageId'  => '{imageId}',
+    'flavorId' => '{flavorId}',
+]);
