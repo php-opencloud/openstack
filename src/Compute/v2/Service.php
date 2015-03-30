a8991d9c5ef429f2fe500f449f4021483e9d1012
Jamie Hannaford <jamie.hannaford@rackspace.com>
2015-03-30 14:18:59 +0200

    Use proper docblock return type and change signature

--- src/Compute/v2/Service.php
+++ src/Compute/v2/Service.php
@@ -6,10 +6,13 @@ use OpenStack\Common\Service\AbstractService;
 
 class Service extends AbstractService
 {
-    public function createServer($name, $imageId, $flavorId, array $options = [])
+    /**
+     * @param array $options
+     *
+     * @return Models\Server
+     */
+    public function createServer(array $options = [])
     {
-        $options = array_merge($options, ['name' => $name, 'flavorId' => $flavorId, 'imageId' => $imageId]);
-
         return $this->model('Server')->create($options);
     }
 }
