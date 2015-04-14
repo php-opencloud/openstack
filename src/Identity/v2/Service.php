00d947417249d9e0868655b2ee40a0dc3599f4d7
Jamie Hannaford <jamie.hannaford@rackspace.com>
2015-04-14 11:20:52 +0200

    Shift to new API structure

--- src/Identity/v2/Service.php
+++ src/Identity/v2/Service.php
@@ -3,13 +3,12 @@
 namespace OpenStack\Identity\v2;
 
 use OpenStack\Common\Service\AbstractService;
-use OpenStack\Identity\v2\Api\Token as TokenApi;
 
 class Service extends AbstractService
 {
     public function generateToken(array $options = [])
     {
-        $response = $this->execute(TokenApi::post(), $options);
+        $response = $this->execute(Api::postToken(), $options);
         return $this->model('Token', $response);
     }
 }
