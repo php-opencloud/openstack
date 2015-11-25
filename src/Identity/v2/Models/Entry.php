php-opencloud/openstack

commit 099ae8b210b3feaa2a1c259f336ac800a6ab9558

refactor model creation to use class keyword

src/Identity/v2/Models/Entry.php
@@ -21,19 +21,6 @@ class Entry extends AbstractResource
     private $endpoints = [];
 
     /**
-     * {@inheritDoc}
-     */
-    public function populateFromArray(array $data)
-    {
-        $this->name = $data['name'];
-        $this->type = $data['type'];
-
-        foreach ($data['endpoints'] as $endpoint) {
-            $this->endpoints[] = $this->model('Endpoint', $endpoint);
-        }
-    }
-
-    /**
      * Indicates whether this catalog entry matches a certain name and type.
      *
      * @param string $name

src/Common/Api/Operator.php
@@ -88,13 +88,9 @@ abstract class Operator implements OperatorInterface
 
     /**
      * {@inheritDoc}
-     *
-     * Refer to {@see getServiceNamespace()} for more information about how model namespaces are resolved.
      */
-    public function model($name, $data = null)
+    public function model($class, $data = null)
     {
-        $class = sprintf("%s\\Models\\%s", $this->getServiceNamespace(), $name);
-
         $model = new $class($this->client, $this->api);
 
         // @codeCoverageIgnoreStart
@@ -113,14 +109,6 @@ abstract class Operator implements OperatorInterface
     }
 
     /**
-     * @return string
-     */
-    public function getCurrentNamespace()
-    {
-        return (new \ReflectionClass(get_class($this)))->getNamespaceName();
-    }
-
-    /**
      * Will create a new instance of this class with the current HTTP client and API injected in. This
      * is useful when enumerating over a collection since multiple copies of the same resource class
      * are needed.
@@ -131,13 +119,4 @@ abstract class Operator implements OperatorInterface
     {
         return new static($this->client, $this->api);
     }
-
-    /**
-     * Determines which root namespace to use when instantiating a new model. For example, if a service class
-     * is invoking the model, it will use ``__NAMESPACE__\Models`` as the root namespace; for models creating
-     * other models, it will just use ``__NAMESPACE__``.
-     *
-     * @return string
-     */
-    abstract protected function getServiceNamespace();
 }

src/Common/Resource/AbstractResource.php
@@ -2,7 +2,6 @@
 
 namespace OpenStack\Common\Resource;
 
-use OpenStack\Common\Api\Operation;
 use OpenStack\Common\Api\Operator;
 use OpenStack\Common\Transport\Utils;
 use Psr\Http\Message\ResponseInterface;
@@ -55,14 +54,6 @@ abstract class AbstractResource extends Operator implements ResourceInterface
     protected $aliases = [];
 
     /**
-     * @codeCoverageIgnore
-     */
-    protected function getServiceNamespace()
-    {
-        return str_replace('\\Models', '', $this->getCurrentNamespace());
-    }
-
-    /**
      * Populates the current resource from a response object.
      *
      * @param ResponseInterface $response
@@ -96,27 +87,39 @@ abstract class AbstractResource extends Operator implements ResourceInterface
             $propertyName = isset($this->aliases[$key]) ? $this->aliases[$key] : $key;
             if (property_exists($this, $propertyName)) {
                 if ($type = $this->extractTypeFromDocBlock($reflClass, $propertyName)) {
-                    if (in_array($type, ['string', 'bool', 'null', 'array', 'object', 'int', 'mixed'])) {
-                        $val = $val;
-                    } elseif (strpos($type, '[]') === 0) {
-                        if (is_array($val)) {
-                            $array = [];
-                            $type = substr($type, 2);
-                            foreach ($val as $subVal) {
-                                array_push($array, $this->model($type, $subVal));
-                            }
-                            $val = $array;
+                    if (strpos($type, '[]') === 0 && is_array($val)) {
+                        $array = [];
+                        foreach ($val as $subVal) {
+                            $array[] = $this->model($this->normalizeModelClass(substr($type, 2)), $subVal);
                         }
-                    } elseif ($type == '\DateTimeImmutable') {
+                        $val = $array;
+                    } elseif (strcasecmp($type, '\datetimeimmutable') === 0) {
                         $val = new \DateTimeImmutable($val);
-                    } elseif (strpos($type, '\\') !== 0) {
-                        $val = $this->model($type, $val);
+                    } elseif ($this->isNotNativeType($type)) {
+                        $val = $this->model($this->normalizeModelClass($type), $val);
                     }
                 }
 
                 $this->$propertyName = $val;
             }
         }
+    }
+
+    private function isNotNativeType($type)
+    {
+        return !in_array($type, [
+            'string', 'bool', 'boolean', 'null', 'array', 'object', 'int', 'integer', 'float', 'numeric', 'mixed'
+        ]);
+    }
+
+    private function normalizeModelClass($class)
+    {
+        if (strpos($class, '\\') === false) {
+            $currentNamespace = (new \ReflectionClass($this))->getNamespaceName();
+            $class = sprintf("%s\\%s", $currentNamespace, $class);
+        }
+
+        return $class;
     }
 
     private function extractTypeFromDocBlock(\ReflectionClass $reflClass, $propertyName)

src/Common/Service/AbstractService.php
@@ -11,13 +11,4 @@ use OpenStack\Common\Api\Operator;
  */
 abstract class AbstractService extends Operator implements ServiceInterface
 {
-    /**
-     * @codeCoverageIgnore
-     *
-     * @return string
-     */
-    protected function getServiceNamespace()
-    {
-        return $this->getCurrentNamespace();
-    }
 }

src/Compute/v2/Models/Flavor.php
@@ -13,11 +13,22 @@ use OpenStack\Common\Resource\Retrievable;
  */
 class Flavor extends AbstractResource implements Listable, Retrievable
 {
+    /** @var int */
     public $disk;
+
+    /** @var string */
     public $id;
+
+    /** @var string */
     public $name;
+
+    /** @var int */
     public $ram;
+
+    /** @var int */
     public $vcpus;
+
+    /** @var array */
     public $links;
 
     protected $resourceKey = 'flavor';

src/Compute/v2/Models/Image.php
@@ -15,30 +15,38 @@ use OpenStack\Common\Transport\Utils;
  */
 class Image extends AbstractResource implements Listable, Retrievable, Deletable
 {
-    public $created;
+    /** @var string */
     public $id;
+
+    /** @var array */
     public $links;
+
+    /** @var array */
     public $metadata;
+
+    /** @var int */
     public $minDisk;
+
+    /** @var int */
     public $minRam;
+
+    /** @var string */
     public $name;
+
+    /** @var string */
     public $progress;
+
+    /** @var string */
     public $status;
+
+    /** @var \DateTimeImmutable */
+    public $created;
+
+    /** @var \DateTimeImmutable */
     public $updated;
 
     protected $resourceKey = 'image';
     protected $resourcesKey = 'images';
-
-    /**
-     * {@inheritDoc}
-     */
-    public function populateFromArray(array $data)
-    {
-        parent::populateFromArray($data);
-
-        $this->created = new \DateTimeImmutable($this->created);
-        $this->updated = new \DateTimeImmutable($this->updated);
-    }
 
     /**
      * {@inheritDoc}

src/Compute/v2/Models/Server.php
@@ -25,23 +25,58 @@ class Server extends AbstractResource implements
 {
     use HasWaiterTrait;
 
+    /** @var string */
     public $id;
+
+    /** @var string */
     public $ipv4;
+
+    /** @var string */
     public $ipv6;
+
+    /** @var array */
     public $addresses;
+
+    /** @var \DateTimeImmutable */
     public $created;
+
+    /** @var \DateTimeImmutable */
     public $updated;
+
+    /** @var Flavor */
     public $flavor;
+
+    /** @var string */
     public $hostId;
+
+    /** @var Image */
     public $image;
+
+    /** @var array */
     public $links;
+
+    /** @var array */
     public $metadata;
+
+    /** @var string */
     public $name;
+
+    /** @var string */
     public $progress;
+
+    /** @var string */
     public $status;
+
+    /** @var string */
     public $tenantId;
+
+    /** @var string */
     public $userId;
+
+    /** @var string */
     public $adminPass;
+
+    /** @var string */
     public $taskState;
 
     protected $resourceKey = 'server';
@@ -57,25 +92,6 @@ class Server extends AbstractResource implements
         'security_groups' => 'securityGroups',
         'OS-EXT-STS:task_state' => 'taskState',
     ];
-
-    /**
-     * {@inheritDoc}
-     */
-    public function populateFromArray(array $data)
-    {
-        parent::populateFromArray($data);
-
-        $this->created = new \DateTimeImmutable($this->created);
-        $this->updated = new \DateTimeImmutable($this->updated);
-
-        if (isset($data['flavor'])) {
-            $this->flavor = $this->model('Flavor', $data['flavor']);
-        }
-
-        if (isset($data['image'])) {
-            $this->image = $this->model('Image', $data['image']);
-        }
-    }
 
     /**
      * {@inheritDoc}

src/Compute/v2/Service.php
@@ -3,6 +3,9 @@
 namespace OpenStack\Compute\v2;
 
 use OpenStack\Common\Service\AbstractService;
+use OpenStack\Compute\v2\Models\Flavor;
+use OpenStack\Compute\v2\Models\Image;
+use OpenStack\Compute\v2\Models\Server;
 
 /**
  * Compute v2 service for OpenStack.
@@ -21,7 +24,7 @@ class Service extends AbstractService
      */
     public function createServer(array $options)
     {
-        return $this->model('Server')->create($options);
+        return $this->model(Server::class)->create($options);
     }
 
     /**
@@ -37,7 +40,7 @@ class Service extends AbstractService
     public function listServers($detailed = false, array $options = [], callable $mapFn = null)
     {
         $def = ($detailed === true) ? $this->api->getServersDetail() : $this->api->getServers();
-        return $this->model('Server')->enumerate($def, $options, $mapFn);
+        return $this->model(Server::class)->enumerate($def, $options, $mapFn);
     }
 
     /**
@@ -52,7 +55,7 @@ class Service extends AbstractService
      */
     public function getServer(array $options = [])
     {
-        $server = $this->model('Server');
+        $server = $this->model(Server::class);
         $server->populateFromArray($options);
         return $server;
     }
@@ -67,7 +70,7 @@ class Service extends AbstractService
      */
     public function listFlavors(array $options = [], callable $mapFn = null)
     {
-        return $this->model('Flavor')->enumerate($this->api->getFlavors(), $options, $mapFn);
+        return $this->model(Flavor::class)->enumerate($this->api->getFlavors(), $options, $mapFn);
     }
 
     /**
@@ -82,7 +85,7 @@ class Service extends AbstractService
      */
     public function getFlavor(array $options = [])
     {
-        $flavor = $this->model('Flavor');
+        $flavor = $this->model(Flavor::class);
         $flavor->populateFromArray($options);
         return $flavor;
     }
@@ -97,7 +100,7 @@ class Service extends AbstractService
      */
     public function listImages(array $options = [], callable $mapFn = null)
     {
-        return $this->model('Image')->enumerate($this->api->getImages(), $options, $mapFn);
+        return $this->model(Image::class)->enumerate($this->api->getImages(), $options, $mapFn);
     }
 
     /**
@@ -112,7 +115,7 @@ class Service extends AbstractService
      */
     public function getImage(array $options = [])
     {
-        $image = $this->model('Image');
+        $image = $this->model(Image::class);
         $image->populateFromArray($options);
         return $image;
     }

commit 8b7ac9a2d7cf9aa9a5c03412cde74072eba6e912

Fix v2 identity private property access bug
The changeset 099ae8b210b3feaa2a1c259f336ac800a6ab9558 broke the
v2 identity api, and caused a private property access issue on
v2's Entry class. This patchset makes the private properties to
public to make it accessible for AbstractResource.populateFromArray()
method.

src/Identity/v2/Models/Entry.php
@@ -12,13 +12,13 @@ use OpenStack\Common\Resource\AbstractResource;
 class Entry extends AbstractResource
 {
     /** @var string */
-    private $name;
+    public $name;
 
     /** @var string */
-    private $type;
+    public $type;
 
     /** @var []Endpoint */
-    private $endpoints = [];
+    public $endpoints = [];
 
     /**
      * Indicates whether this catalog entry matches a certain name and type.