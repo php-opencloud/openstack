5b5d0d9fda20c8a515a00ee0ca5b0c5051148b7c
Ha Phan <thanhha.work@gmail.com>
2016-04-16 11:16:55 +0800

    Add samples for security group and security group rule

---
+++ samples/networking/v2ext/security_group/create_security_group.php
@@ -0,0 +1,28 @@
+<?php
+
+use OpenStack\Networking\v2\Extensions\SecurityGroups\Models\SecurityGroup;
+
+require 'vendor/autoload.php';
+
+$openstack = new OpenStack\OpenStack([
+    'authUrl' => '{authUrl}',
+    'region'  => '{region}',
+    'user'    => [
+        'id'       => '{userId}',
+        'password' => '{password}'
+    ],
+    'scope' => [
+        'project' => [
+            'id' => '{projectId}'
+        ]
+    ]
+]);
+
+$networkingExtSecGroup = $openstack->networkingV2ExtSecGroups();
+
+/** @var SecurityGroup $securityGroup */
+$securityGroup = $networkingExtSecGroup->createSecurityGroupRule([
+    'name' => 'New SecGroup',
+    'description' => 'Foo Barrr'
+]);
+
---
+++ samples/networking/v2ext/security_group/delete_security_group.php
@@ -0,0 +1,24 @@
+<?php
+
+use OpenStack\Networking\v2\Extensions\SecurityGroups\Models\SecurityGroup;
+
+require 'vendor/autoload.php';
+
+$openstack = new OpenStack\OpenStack([
+    'authUrl' => '{authUrl}',
+    'region'  => '{region}',
+    'user'    => [
+        'id'       => '{userId}',
+        'password' => '{password}'
+    ],
+    'scope' => [
+        'project' => [
+            'id' => '{projectId}'
+        ]
+    ]
+]);
+
+$networkingExtSecGroup = $openstack->networkingV2ExtSecGroups();
+
+$securityGroup = $networkingExtSecGroup->getSecurityGroup('{id}');
+$securityGroup->delete();
