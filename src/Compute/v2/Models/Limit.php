35266385c854a940c45acad02f1efd9a8cec4f9e
Ha Phan <thanhha.work@gmail.com>
2016-04-24 12:13:48 +0800

    Rename model to Limit

--- src/Compute/v2/Models/Limit.php
+++ src/Compute/v2/Models/Limit.php
@@ -2,13 +2,9 @@
 
 namespace OpenStack\Compute\v2\Models;
 
-use OpenCloud\Common\Resource\Creatable;
 use OpenCloud\Common\Resource\OperatorResource;
-use OpenCloud\Common\Resource\Retrievable;
-use OpenCloud\Common\Transport\Utils;
-
 /**
- * Represents a Compute v2 Quota
+ * Represents a Compute v2 Limit
  *
  * @property \OpenStack\Compute\v2\Api $api
  */
