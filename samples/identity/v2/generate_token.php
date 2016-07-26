7de4609018031b01e47f4356f5d6360f60f66758
Jamie Hannaford <jamie.hannaford@rackspace.com>
2016-07-26 16:24:12 +0800

    Update generate_token.php

--- samples/identity/v2/generate_token.php
+++ samples/identity/v2/generate_token.php
@@ -1,12 +1,25 @@
 <?php
 
-require __DIR__ . '/../../../vendor/autoload.php';
+require 'vendor/autoload.php';
 
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use OpenStack\Identity\v2\Service;
+use OpenStack\Common\Transport\Utils as TransportUtils;
 use OpenStack\OpenStack;
 
-$client = new OpenStack;
-
-$objectStore = $client->objectStoreV2([
-    'region' => 'RegionOne',
-    //'debug' => true,
+$httpClient = new Client([
+    'base_uri' => TransportUtils::normalizeUrl(getenv('OS_AUTH_URL')),
+    'handler'  => HandlerStack::create(),
 ]);
+        
+$options = [
+    'authUrl'         => 'https://example.com:5000/v2.0',
+    'region'          => 'RegionOne',
+    'username'        => 'foo',
+    'password'        => 'bar',
+    'tenantName'      => 'baz',
+    'identityService' => Service::factory($httpClient),
+];
+
+$openstack = new OpenStack($options);
