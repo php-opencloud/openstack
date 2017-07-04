5f2a461d4e8a99515217ec151a6ba8890fa6b6d2
Ha Phan <thanhha.work@gmail.com>
2017-07-04 17:48:18 +0800

    Added draft sdk for Gnocchi

---
+++ src/Metric/v1/Gnocchi/Params.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace OpenStack\Metric\v1\Gnocchi;
+
+use OpenStack\Common\Api\AbstractParams;
+
+class Params extends AbstractParams
+{
+    public function resourceType(): array
+    {
+        return [
+            'location'    => self::URL,
+            'type'        => self::STRING_TYPE,
+            'description' => 'Resource type',
+            'required'    => true,
+        ];
+    }
+
+    public function sort(): array
+    {
+        return [
+            'location'    => self::QUERY,
+            'type'        => self::STRING_TYPE,
+            'description' => 'Sort criteria',
+        ];
+    }
+
+    public function criteria(): array
+    {
+        return [
+            'location'    => self::RAW,
+            'type'        => self::STRING_TYPE,
+            'description' => 'Filter resources based on attributes values. See http://gnocchi.xyz/stable_4.0/rest.html#searching-for-resources',
+        ];
+    }
+
+    public function headerContentType(): array
+    {
+        return [
+            'location'    => self::HEADER,
+            'type'        => self::STRING_TYPE,
+            'sentAs'      => 'Content-Type',
+            'description' => 'Override request header',
+            'documented'  => false,
+        ];
+    }
+
+    public function idUrl($type)
+    {
+        return [
+            'required'    => true,
+            'location'    => self::URL,
+            'description' => sprintf('The unique ID, or identifier, for the %s', $type)
+        ];
+    }
+}
---
+++ src/Metric/v1/Gnocchi/Service.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types=1);
+
+namespace OpenStack\Metric\v1\Gnocchi;
+
+use OpenStack\Common\Service\AbstractService;
+use OpenStack\Metric\v1\Gnocchi\Models\Metric;
+use OpenStack\Metric\v1\Gnocchi\Models\Resource;
+use OpenStack\Metric\v1\Gnocchi\Models\ResourceType;
+
+
+/**
+ * Gnocci Metric v1 Service class
+ *
+ * @property Api $api
+ *
+ * @package OpenStack\Metric\v1\Gnocchi
+ */
+class Service extends AbstractService
+{
+    public function listResourceTypes(): \Generator
+    {
+        return $this->model(ResourceType::class)->enumerate($this->api->getResourceTypes(), []);
+    }
+
+    public function listResources(array $options = []): \Generator
+    {
+        $this->injectGenericType($options);
+
+        return $this->model(Resource::class)->enumerate($this->api->getResources(), $options);
+    }
+
+    public function getResource(array $options = []): Resource
+    {
+        $this->injectGenericType($options);
+
+        $resource = $this->model(Resource::class);
+        $resource->populateFromArray($options);
+
+        return $resource;
+    }
+
+    public function searchResources(array $options = []): \Generator
+    {
+        /**
+         * $options['criteria'] must send as STRING
+         * This will check input $options and perform json_encode if needed.
+         */
+        if (isset($options['criteria']) && !is_string($options['criteria'])) {
+            $options['criteria'] = json_encode($options['criteria']);
+        }
+
+        /**
+         * We need to manually add content-type header to this request
+         * since searchResources method sends RAW request body.
+         */
+        $options['contentType'] = 'application/json';
+
+        return $this->model(Resource::class)->enumerate($this->api->searchResources(), $options);
+    }
+
+    public function getMetric($id): Metric
+    {
+        $metric = $this->model(Metric::class);
+        $metric->populateFromArray(['id' => $id]);
+
+        return $metric;
+    }
+
+    public function listMetrics(array $options = []): \Generator
+    {
+        return $this->model(Metric::class)->enumerate($this->api->getMetrics(), $options);
+    }
+
+    private function injectGenericType(array &$options)
+    {
+        if (empty($options) || !isset($options['type'])) {
+            $options['type'] = Resource::RESOURCE_TYPE_GENERIC;
+        }
+    }
+}
