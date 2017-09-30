77c025ca46729c7fa6470d6c22ea485fad88b0cc
Ekasit Kijsipongse <ekasit.kijsipongse@nectec.or.th>
2017-09-30 21:21:21 +0700

    Add create_multiple_clusters.php

---
+++ samples/data_processing/cluster/create_multiple_clusters.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use OpenStack\OpenStack;
+
+$openstack = new OpenStack([
+    'authUrl' => '{authUrl}',
+    'user'    => [
+        'name'     => '{userName}',
+        'password' => '{password}',
+        'domain'   => ['name' => '{userDomain}'],
+    ],
+    'scope'   => [
+        'project'  => [
+             'name'   => '{projectName}',
+             'domain' => ['name' => '{projectDomain}'],
+        ],
+    ],
+]);
+
+$sahara = $openstack->dataProcessingV1(['region' => '{region}']);
+
+$options = [
+    'pluginName'               => '{pluginName}',
+    'hadoopVersion'            => '{hadoopVersion}',
+    'clusterTemplateId'        => '{clusterTemplateId}',
+    'defaultImageId'           => '{defaultImageId}',
+    //user keypair id is optional
+    'userKeypairId'            => '{userKeypairId}',
+    'name'                     => '{name}',
+    'neutronManagementNetwork' => '{neutronManagementNetworkId}',
+    'count'                    => '{count}',
+    //optional
+    'clusterConfigs'           => ['foo' => 'bar'],
+];
+
+$clusters = $sahara->createCluster($options);
+foreach ($clusters as $cluster) {
+    print_r($cluster);
+}
