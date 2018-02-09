783a6cc7699b2c277b36403cd90504500a17c10f
Ha Phan <thanhha.work@gmail.com>
2018-02-09 18:10:10 +0800

    Fixed authentication v2 bug

--- samples/identity/v2/authentication.php
+++ samples/identity/v2/authentication.php
@@ -5,9 +5,15 @@ require 'vendor/autoload.php';
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use OpenStack\Common\Transport\Utils as TransportUtils;
+use OpenStack\Identity\v2\Service;
 use OpenStack\OpenStack;
 
 $authUrl = 'https://keystone.example.com:5000/v2.0';
+
+$httpClient = new Client([
+    'base_uri' => TransportUtils::normalizeUrl($authUrl),
+    'handler'  => HandlerStack::create(),
+]);
 
 $options = [
     'authUrl'         => $authUrl,
@@ -15,12 +21,7 @@ $options = [
     'username'        => 'foo',
     'password'        => 'bar',
     'tenantName'      => 'baz',
-    'identityService' => new Client(
-        [
-            'base_uri' => TransportUtils::normalizeUrl($authUrl),
-            'handler'  => HandlerStack::create(),
-        ]
-    ),
+    'identityService' => Service::factory($httpClient),
 ];
 
 /** @var OpenStack $openstack */
