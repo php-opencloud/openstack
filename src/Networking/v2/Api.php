797e8d70960fad531110adc9431acf1651d68c23
Omid Pilevar <omid@arvancloud.com>
2019-11-20 19:22:04 +0330

    add fixedIps in list ports

--- src/Networking/v2/Api.php
+++ src/Networking/v2/Api.php
@@ -209,6 +209,7 @@ class Api extends AbstractApi
                 'securityGroups' => $this->params->secGroupsQuery(),
                 'deviceId'       => $this->params->deviceIdQuery(),
                 'ipAllocation'   => $this->params->ipAllocationQuery(),
+                'fixedIps'       => $this->params->fixedIps(),
             ],
         ];
     }
