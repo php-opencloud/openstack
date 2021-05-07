c51e52d3535352467361aa85b261663307ea7319
Brais de Sousa <braisdesousa@gmail.com>
2021-05-07 10:34:16 +0200

    [DEV]

--- src/Identity/v2/Api.php
+++ src/Identity/v2/Api.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace OpenStack\Identity\v2;
 
-use App\Service\Rackspace\Params;
 use OpenStack\Common\Api\ApiInterface;
 
 /**
---
+++ src/Identity/v2/Params.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace OpenStack\Identity\v2;
+
+
+use OpenStack\Common\Api\AbstractParams;
+
+class Params extends AbstractParams
+{
+    public function username()
+    {
+        return [
+            'type'     => self::STRING_TYPE,
+            'location' => self::JSON,
+            'path'     => 'auth.RAX-KSKEY:apiKeyCredentials',
+            'required' => true,
+        ];
+    }
+
+    public function apiKey()
+    {
+        return [
+            'type'     => self::STRING_TYPE,
+            'location' => self::JSON,
+            'path'     => 'auth.RAX-KSKEY:apiKeyCredentials',
+            'required' => true,
+        ];
+    }
+}
